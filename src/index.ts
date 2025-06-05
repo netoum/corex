5f2b1faaa4cb8e92a08bdcb2a0a6ed0104179835
karimsemmoud <karim.semmoud@netoum.com>
2025-06-05 21:55:59 +0200

    0.0.3

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,17 @@ import { Accordion } from "./components/accordion";
 import { Avatar } from "./components/avatar";
 import { Checkbox } from "./components/checkbox";
 import { Clipboard } from "./components/clipboard";
+import { Code }  from "./components/code";
 import { Collapsible } from "./components/collapsible";
+import { DatePicker } from "./components/date-picker";
 import { Dialog } from "./components/dialog";
 import { Listbox } from "./components/listbox";
 import { Menu } from "./components/menu";
 import { ZagSwitch } from "./components/switch";
 import { Switcher } from "./components/switcher";
 import { Tabs } from "./components/tabs";
+import { Timer } from "./components/timer";
 import { ToggleGroup } from "./components/toggle-group";
 import { TreeView } from "./components/tree-view";
 
-export { Accordion, Avatar, Checkbox, Clipboard, Collapsible, Dialog, Listbox, Menu, Switcher, Tabs, ToggleGroup, TreeView, ZagSwitch };
+export { Accordion, Avatar, Checkbox, Clipboard, Code, Collapsible, DatePicker, Dialog, Listbox, Menu, Switcher, Tabs, Timer, ToggleGroup, TreeView, ZagSwitch };
